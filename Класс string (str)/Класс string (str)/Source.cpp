7ed0868c3a30c046bd2b9e0a71b776e27d25a8ce
Micronik-PS <mic.art.ron@gmail.com>
2020-05-29 15:35:09 +0300

    Улучшенная версия класса

--- Класс string (str)/Класс string (str)/Source.cpp
+++ Класс string (str)/Класс string (str)/Source.cpp
@@ -1,136 +1,115 @@
 #include <iostream>
 using namespace std;
 //Class string
-class str
+class MyString
 {
-	int size=0;
-	char* word;
-
 public:
-	//Constructor (standart)
-	str() {
-		word = new char[size+1];
-		word[0] = '\0';
-	}
-	//Constructor copy
-	str(const str& other)
-	{
-		size = other.size;
-		word = new char[size + 1];
-		for (int i = 0; i < size + 1; i++)
+	// MyString A = "Text";
+	MyString(const char*const str) {
+		int size = strlen(str);
+		this->str = new char[size+1];
+		for (int i = 0; i < size; i++)
 		{
-			word[i] = other.word[i];
+			(this->str)[i] = str[i];
 		}
+		(this->str)[size] = '\0';
 	}
-	//Constructor char
-	str(const char* const arr) {
-		size = strlen(arr);
-		word = new char[size+1];
-		for (int i = 0; i < size+1; i++)
+	// MyString A = B;
+	MyString(const MyString& other) {
+		int size = strlen(other.str);
+		str = new char[size + 1];
+		for (int i = 0; i < size; i++)
 		{
-			word[i] = arr[i];
+			str[i] = (other.str)[i];
 		}
+		str[size] = '\0';
 	}
+	// MyString A;
+	MyString() {
+		int size = 0;
+		str = new char[size + 1];
+		str[size] = '\0';
+	}
+	MyString& operator = (const char* const str) {
+		Dead();
+		int size = strlen(str);
+		this->str = new char[size + 1];
+		for (int i = 0; i < size; i++)
+		{
+			(this->str)[i] = str[i];
+		}
+		(this->str)[size] = '\0';
+		return *this;
+	}
+	// A = B;
+	MyString& operator = (const MyString& other) {
+		Dead();
+		int size = strlen(other.str);
+		str = new char[size + 1];
+		for (int i = 0; i < size; i++)
+		{
+			str[i] = (other.str)[i];
+		}
+		str[size] = '\0';
+		return *this;
+	}
+	// A + "Text"
+	MyString operator + (const char*const str) {
+		char Duo_Str_Arr[255];
+		int j = 0;
+		int size_object = strlen(this->str);
+		int size_str = strlen(str);
+		for (int i = 0; i < size_object;i++,j++) {
+			Duo_Str_Arr[j] = (this->str)[i];
+		}
+		for (int i = 0; i < size_str; i++, j++) {
+			Duo_Str_Arr[j] = str[i];
+		}
+		Duo_Str_Arr[size_object + size_str] = '\0';
+		MyString Duo_Str = Duo_Str_Arr;
+		return Duo_Str;
+	}
+	// A + B
+	MyString operator + (const MyString& other) {
+		char Duo_Str_Arr[255];
+		int j = 0;
+		int size_this_object = strlen(str);
+		int size_other_object = strlen(other.str);
+		for (int i = 0; i < size_this_object; i++, j++) {
+			Duo_Str_Arr[j] = str[i];
+		}
+		for (int i = 0; i < size_other_object; i++, j++) {
+			Duo_Str_Arr[j] = other.str[i];
+		}
+		Duo_Str_Arr[size_this_object + size_other_object] = '\0';
+		MyString Duo_Str = Duo_Str_Arr;
+		return Duo_Str;
+	}
+	
 
-	//Overload operator = for string
-	void operator=(const char* const arr)
-	{
-		if (!(word == nullptr))
+	// Output str;
+	void print() {
+		int size = strlen(str);
+		for (int i = 0; i < size; i++)
 		{
-			delete[] word;
-			word = nullptr;
-		}
-		size = strlen(arr);
-		word = new char[size+1];
-		for (int i = 0; i < size+1; i++)
-		{
-			word[i] = arr[i];
+			cout << str[i];
 		}
 	}
-	//Overload operator = for str
-	char* operator=(const str other)
-	{
-		if (!(word == nullptr))
-		{
-			delete[] word;
-			word = nullptr;
-		}
-		size = other.size;
-		word = new char[size+1];
-		for (int i = 0; i < size+1; i++)
-		{
-			word[i] = other.word[i];
-		}
-		return word;
+	// Deconstructor
+	~MyString() {
+		delete[]str;
+		str = nullptr;
 	}
-	//Operator overload + for string
-	char* operator+(const char*const arr)
-	{
-		int size = strlen(word) + strlen(arr);
-		char* dual_arr = new char[size+1];
-		int i = 0;
-		int j = 0;
-		while (word[j] != '\0')
-		{
-			dual_arr[i] = word[j];
-			i++;
-			j++;
+private:
+	char* str=nullptr;
+	//Check Alive Word
+	void Dead() {
+		if (str != nullptr) {
+			delete[]str;
 		}
-		j = 0;
-		while (arr[j] != '\0')
-		{
-			dual_arr[i] = arr[j];
-			i++;
-			j++;
-		}
-		dual_arr[size] = '\0';
-		return dual_arr;
-	}
-	//Operator overload + for str
-	char* operator+(const str& other)
-	{
-		int size = strlen(word) + strlen(other.word);
-		char* dual_arr = new char[size+1];
-		int i = 0;
-		int j = 0;
-		while (word[j] != '\0')
-		{
-			dual_arr[i] = word[j];
-			i++;
-			j++;
-		}
-		j = 0;
-		while (other.word[j] != '\0')
-		{
-			dual_arr[i] = other.word[j];
-			i++;
-			j++;
-		}
-		dual_arr[size] = '\0';
-		return dual_arr;
-	}
-	//Get_Size
-	int Get_Size()
-	{
-		return size;
-	}
-	//Get_Word
-	char* Get_Word()
-	{
-		return word;
-	}
-	//Deconstructor, delete Dynamic array with memory
-	~str()
-	{
-		delete[] word;
-		word = nullptr;
 	}
 };
-//-------------------------------
-int main()
-{
-	str a;
-	a = "A";
-	str b = a;
-	cout << b.Get_Word();
+
+int main() {
+
 }
